12821e719c2841acc5d531efa37e41af1506e0d3
Dylan Rolfe <mr.dylan.rolfe@gmail.com>
2026-05-25 16:27:14 +1000

    Created a Recipe List entity. Added an 'Add to list' button. Removed IngredientFormatter. Added js/build folder with AddIngredient widget build.

--- src/EventSubscriber/RecipesEntitySubscriber.php
+++ src/EventSubscriber/RecipesEntitySubscriber.php
@@ -4,6 +4,7 @@ namespace Drupal\recipes\EventSubscriber;
 
 use Drupal\core_event_dispatcher\EntityHookEvents;
 use Drupal\core_event_dispatcher\Event\Entity\EntityPredeleteEvent;
+use Drupal\core_event_dispatcher\Event\Entity\EntityViewAlterEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -11,14 +12,17 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  *
  * @package Drupal\recipes\RecipesEntitySubscriber
  */
-class RecipesEntitySubscriber implements EventSubscriberInterface  {
+class RecipesEntitySubscriber implements EventSubscriberInterface
+{
 
   /**
    * {@inheritdoc}
    */
-  public static function getSubscribedEvents(): array {
+  public static function getSubscribedEvents(): array
+  {
     return [
       EntityHookEvents::ENTITY_PRE_DELETE => 'onEntityDelete',
+      EntityHookEvents::ENTITY_VIEW_ALTER => 'onEntityViewAlter'
     ];
   }
 
@@ -28,12 +32,13 @@ class RecipesEntitySubscriber implements EventSubscriberInterface  {
    * @param \Drupal\core_event_dispatcher\Event\Entity\EntityPredeleteEvent $event
    *   The event.
    */
-  public function onEntityDelete(EntityPredeleteEvent $event): void {
+  public function onEntityDelete(EntityPredeleteEvent $event): void
+  {
     // Remove all the ingredients associated with a recipe.
 
     // Only act if we are deleting a 'recipe' node.
     if ($event->getEntity()->getEntityTypeId() === 'node' && $event->getEntity()->bundle() === 'recipes_recipe') {
-      
+
       // Find all 'ingredient' nodes that reference this recipe.
       $ingredients = $event->getEntity()->get('field_recipes_ingredients')->referencedEntities();
 
@@ -43,8 +48,42 @@ class RecipesEntitySubscriber implements EventSubscriberInterface  {
           $ingredient->delete();
         }
       }
+
+      // Remove recipe from any lists.
+      $storage = \Drupal::entityTypeManager()->getStorage('recipes_recipe_list');
+      $lists = $storage->loadMultiple();
+      foreach ($lists as $list) {
+        foreach ($list->get('recipes')->referencedEntities() as $delta => $recipe) {
+          if ($recipe->id() == $event->getEntity()->id()) {
+            $list->get('recipes')->removeItem($delta);
+          }
+        }
+        $list->save();
+      }
     }
   }
 
+  /**
+   * Entity view alter.
+   *
+   * @param \Drupal\core_event_dispatcher\Event\Entity\EntityViewAlterEvent $event
+   *   The event.
+   */
+  public function onEntityViewAlter(EntityViewAlterEvent $event): void
+  {
+    // Add an 'Add to list' button at the bottom of Recipe nodes.
+    if ($event->getEntity()->getEntityTypeId() === 'node' && $event->getEntity()->bundle() === 'recipes_recipe') {
+      // Check that this recipe isn't already in the list.
 
+      $build = &$event->getBuild();
+
+      $build['add_to_list_button'] = [
+        '#theme' => 'add_to_list_button',
+        '#label' => 'Add to list',
+        '#id' => $event->getEntity()->id(),
+        '#weight' => 100
+      ];
+      
+    }
+  }
 }
